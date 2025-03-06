9193ff602b96c99c9743cbaf5e25583c73e3990f
senpy <fivoszan@email.com>
2025-03-06 12:31:47 +0200

    vc

--- pyode.cpp
+++ pyode.cpp
@@ -2,13 +2,9 @@
 
 #include <iostream>
 
-#ifndef N
-#define N -1  // Default value if not provided
-#endif
-
 PYBIND11_MODULE(odepack, m) {
-    define_ode_module<double, Eigen::Array<double, 1, N>>(m);
+    define_ode_module<double, Eigen::Array<double, 1, -1>>(m);
 }
 
 
-//g++ -DN=-1 -O3 -Wall -shared -std=c++20 -fopenmp -I/usr/include/python3.12 -I/usr/include/pybind11 -fPIC $(python3 -m pybind11 --includes) pyode.cpp -o odepack$(python3-config --extension-suffix)
+//g++ -O3 -Wall -shared -std=c++20 -fopenmp -I/usr/include/python3.12 -I/usr/include/pybind11 -fPIC $(python3 -m pybind11 --includes) pyode.cpp -o odepack$(python3-config --extension-suffix)
