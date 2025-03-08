0e97c4fa13569ceb60bc1ee950b0673a4e14fc25
senpy <fivoszan@email.com>
2025-03-08 14:02:47 +0200

    sdv

--- pyode.cpp
+++ pyode.cpp
@@ -5,8 +5,3 @@
 PYBIND11_MODULE(odepack, m) {
     define_ode_module<double, vec<double>>(m);
 }
-
-
-//g++ -O3 -Wall -shared -std=c++20 -fopenmp -I/usr/include/python3.12 -I/usr/include/pybind11 -fPIC $(python3 -m pybind11 --includes) pyode.cpp -o odepack$(python3-config --extension-suffix)
-
-//g++ -O3 -march=native -flto -Wall -shared -std=c++20 -fopenmp -I/usr/include/python3.12 -I/usr/include/pybind11 -fPIC $(python3 -m pybind11 --includes) -fno-math-errno bohm.cpp -o bohm$(python3-config --extension-suffix)
