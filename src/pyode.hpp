d0467d3b10dac264b188925ce8d77cfd6c8582fa
senpy <fivoszan@email.com>
2025-03-10 01:55:00 +0200

    dfv

--- src/pyode.hpp
+++ src/pyode.hpp
@@ -375,7 +375,7 @@ void define_ode_module(py::module& m) {
     m.def("integrate_all", [](py::object list, const Tt& interval, const int& max_frames, const int& max_events, const bool& terminate){
         std::vector<ODE<Tt, Ty>*> array;
         for (const py::handle& item : list){
-            array.push_back(&(item.cast<ODE<Tt, Ty>&>()));
+            array.push_back(&(item.cast<PyODE<Tt, Ty>&>()));
         }
         integrate_all(array, interval, max_frames, max_events, terminate);
     }, py::arg("ode_array"), py::arg("interval"), py::arg("max_frames")=-1, py::arg("max_events")=-1, py::arg("terminate")=true);
