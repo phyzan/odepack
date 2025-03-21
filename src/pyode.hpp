c399290d87cb60a1a70924afe41c37723f07e288
senpy <fivoszan@gmail.com>
2025-03-21 02:32:53 +0200

    sdf

--- src/pyode.hpp
+++ src/pyode.hpp
@@ -231,6 +231,7 @@ Func<Tt, Ty> to_Func(py::object f, const _Shape& shape, py::tuple py_args){
     }
     else{
         g = [f, shape, py_args](const Tt& t, const Ty& y, const std::vector<Tt>& _) -> Ty {
+            std::cout << shape[0] << " " << shape[1] << " ";
             return fast_convert<Tt, Ty>(f(t, to_numpy<Tt>(y, shape), *py_args));
         };
     }
