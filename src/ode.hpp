8230530103862bb14355bbac17b648911a7f7f8c
senpy <fivoszan@gmail.com>
2025-03-21 02:24:40 +0200

    sdf

--- src/ode.hpp
+++ src/ode.hpp
@@ -190,6 +190,7 @@ const OdeResult<Tt, Ty> ODE<Tt, Ty>::integrate(const Tt& interval, const int& ma
 template<class Tt, class Ty>
 const OdeResult<Tt, Ty> ODE<Tt, Ty>::go_to(const Tt& t, const int& max_frames, const int& max_events, const bool& terminate, const int& max_prints, const bool& include_first){
     auto t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "hiii11" << std::endl;
     const Tt t0 = _solver->t();
     const Tt interval = t-t0;
     const size_t N = _t_arr.size();
@@ -198,10 +199,12 @@ const OdeResult<Tt, Ty> ODE<Tt, Ty>::go_to(const Tt& t, const int& max_frames, c
     size_t i = N;
     const int MAX_PRINTS = max_prints;
     int prints = 0;
+    std::cout << "hiii1122" << std::endl;
     _solver->reopen_file();
     _solver->set_goal(t);
+    std::cout << "hiii11333" << std::endl;
     while (_solver->is_running()){
-        std::cout << "hiii11" << std::endl;
+        std::cout << "hiii11444" << std::endl;
         if (_solver->advance()){
             std::cout << "hiii2" << std::endl;
             if ((event_counter != max_events) && _solver->at_event()){
